a365052c445dc9cabc6a2fdfa50e81a624b8606a
caiki <caiki.13@gmail.com>
2018-06-13 11:30:47 +0000

    last1

--- ProjectIoT/Procesos/admin.py
+++ ProjectIoT/Procesos/admin.py
@@ -2,10 +2,6 @@ from django.contrib import admin
 from .models import *
 # Register your models here.
 
-
-
-admin.site.register(Inicio)
-
 """class DepositoAdmin(admin.ModelAdmin):
 	list_display = ('idDeposito','ReporteDiario','NroBoucher','Concepto','Monto')
 	pass
